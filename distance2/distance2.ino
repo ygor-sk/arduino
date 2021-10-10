d7f8e7e244bc0137f6373a547e78396dc4489ff7
Igor Inas <igor.inas@gmail.com>
2021-10-10 13:55:29 +0200

    Distance with notes

--- distance2/distance2.ino
+++ distance2/distance2.ino
@@ -1,10 +1,4 @@
-// ---------------------------------------------------------------- //
-// Arduino Ultrasoninc Sensor HC-SR04
-// Re-writed by Arbi Abdul Jabbaar
-// Using Arduino IDE 1.8.7
-// Using HC-SR04 Module
-// Tested on 17 September 2019
-// ---------------------------------------------------------------- //
+#include "pitches.h"
 
 #define echoPin 2 // attach pin D2 Arduino to pin Echo of HC-SR04
 #define trigPin 3 //attach pin D3 Arduino to pin Trig of HC-SR04
@@ -13,6 +7,13 @@
 long duration; // variable for the duration of sound wave travel
 int distance; // variable for the distance measurement
 
+int notes [] = {NOTE_C2, NOTE_D2, NOTE_E2, NOTE_F2, NOTE_G2, NOTE_A2, NOTE_B2, NOTE_C3};
+
+int newNoteCandidateValue = -1;
+int newNoteCandidateCount = 0;
+
+int note = -1;
+
 void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
   pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
@@ -20,6 +21,7 @@ void setup() {
   Serial.println("Ultrasonic Sensor HC-SR04 Test"); // print some text in Serial Monitor
   Serial.println("with Arduino UNO R3");
 }
+
 void loop() {
   // Clears the trigPin condition
   digitalWrite(trigPin, LOW);
@@ -37,17 +39,25 @@ void loop() {
   Serial.print(distance);
   Serial.println(" cm");
 
-  if (distance > 500) {
-    noTone(8);
-  } else {
-    int t = distance * 2 + 40;
-    Serial.println(t);
-  
-   tone(8, t);
+    int newNote = distance / 20;
+
+    if (newNote == newNoteCandidateValue) {
+      newNoteCandidateCount++;
+    } else {
+      newNoteCandidateValue = newNote;
+      newNoteCandidateCount = 0;
+    }
+
+    if (newNoteCandidateCount > 3) {
+      note = newNote;
+    }
     
-  }
-
+    Serial.println(note);
+    if (note >= 0 && note < sizeof(notes) / sizeof(int)) {
+      tone(8, notes[note] * 2);
+    } else {
+      noTone(8);
+    }
   
-  delay(50);
-
+  delay(10);
 }
